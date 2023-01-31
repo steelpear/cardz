7d14ea05f1771e12cb35eb09eeeda2bc4b068a33
steelpear <steelpear@gmail.com>
2023-01-31 12:04:11 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
 import {useState, useEffect} from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import { ProgressSpinner } from 'primereact/progressspinner'
 import styles from '@/styles/Home.module.css'
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
-import 'primereact/resources/primereact.css'
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
+import { Loader } from '../components/Loader'
 
 export default function Home () {
   const [cards, setCards] = useState([])
   const [loading, setLoading] = useState(true)
-  const [height, setHeight] = useState(null)
 
   useEffect(() => {
     async function load() {
@@ -20,25 +15,10 @@ export default function Home () {
       setCards(json)
       setLoading(false)
     }
-    setHeight(window.innerHeight - 20)
     load()
   }, [])
 
-  console.log(height)
-
-  if (loading && height) { return (
-    <>
-      <Head>
-        <title>Анкеты объектов</title>
-        <meta name="description" content="Анкеты объектов | Инструменты администратора" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="card flex justify-content-center align-items-center" style={{ height: height}}>
-        <ProgressSpinner style={{width: '120px', height: '120px'}} strokeWidth="3" />
-      </div>
-    </>
-  )}
+  if (loading) {return (<Loader />)}
 
   return (
     <>
