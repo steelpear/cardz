35319cee54c9fa6acb20edb128590576e318112c
steelpear <steelpear@gmail.com>
2023-02-04 07:12:42 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,14 @@ import { Loader } from '../components/Loader'
 import {MainLayout} from '../components/MainLayout'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
+import { InputText } from 'primereact/inputtext'
+import { FilterMatchMode } from 'primereact/api'
         
 export default function Home () {
   const [cards, setCards] = useState([])
   const [loading, setLoading] = useState(true)
+  const [filters, setFilters] = useState({'global': { value: null, matchMode: FilterMatchMode.CONTAINS }})
+  const [globalFilterValue, setGlobalFilterValue] = useState('')
 
   useEffect(() => {
     async function load() {
@@ -21,15 +25,35 @@ export default function Home () {
 
   if (loading) {return (<Loader />)}
 
+  const onGlobalFilterChange = (e) => {
+    const value = e.target.value
+    let _filters = { ...filters }
+    _filters['global'].value = value
+    setFilters(_filters)
+    setGlobalFilterValue(value)
+  }
+
+  const renderHeader = () => {
+    return (
+      <div className="flex justify-content-end">
+        <span className='p-input-icon-left p-input-icon-right'>
+          <i className="pi pi-search" />
+          <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Поиск" />
+          <i className="pi pi-times" />
+        </span>
+      </div>
+    )
+  }
+
+  const header = renderHeader()
+
   return (
     <MainLayout>
       <main className={styles.main}>
-        <div className="card">
-          <DataTable value={cards} size="small" stripedRows removableSort paginator responsiveLayout="scroll" paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown" currentPageReportTemplate="Строки {first} - {last} из {totalRecords}" rows={50} rowsPerPageOptions={[50,100,cards.length]}>
-            <Column field="name" header="Название объекта" sortable></Column>
-            <Column field="city" header="Город / Регион" sortable></Column>
-          </DataTable>
-        </div>
+        <DataTable value={cards} size="small" stripedRows removableSort paginator responsiveLayout="scroll" paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown" currentPageReportTemplate="Строки {first} - {last} из {totalRecords}" rows={50} rowsPerPageOptions={[50,100,cards.length]} filters={filters} filterDisplay="row" globalFilterFields={['name', 'city']} header={header} emptyMessage="Ничего не найдено." style={{'width': '95%'}}>
+          <Column field="name" header="Название объекта" sortable style={{'width': '70%'}}></Column>
+          <Column field="city" header="Город / Регион" sortable style={{'width': '30%'}}></Column>
+        </DataTable>
       </main>
     </MainLayout>
   )
