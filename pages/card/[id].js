99554113f24c120270ac1d6ef69f0a105556d7b7
steelpear <steelpear@gmail.com>
2023-02-06 17:26:57 +0300

    add scrolling

--- pages/card/[id].js
+++ pages/card/[id].js
@@ -1,17 +1,47 @@
+import {useState, useEffect} from 'react'
 import {MainLayout} from '../../components/MainLayout'
-import {useRouter} from 'next/router'
+import { Link, animateScroll as scroll } from 'react-scroll'
 import styles from '@/styles/Card.module.css'
 
 export default function Card({card}) {
-  const router = useRouter()
-
-  console.log(router)
+  const [currentTab, setcurrentTab] = useState(0)
 
   return(
     <MainLayout>
       <main className={styles.main}>
-        <h1 style={{textAlign:'center'}}>{card.name}</h1>
-        <div dangerouslySetInnerHTML={{__html: card.description}} />
+        <h1 style={{textAlign:'center', marginTop: '55px'}}>{card.name}</h1>
+        {card.sections && card.sections.length > 0 ? <>
+          <div className={styles.tabs_wrap}>
+            {card.sections.map((section, index) => {
+              return (
+                <div className={styles.tab} key={index}>
+                  {/* <Link href={`#${index}`} className={currentTab === index ? styles.active : styles.non_active} onClick={() => setcurrentTab(index)}>{section.tabName}</Link> */}
+                  <Link
+                    activeClass={styles.active}
+                    className={styles.non_active}
+                    to={`#${index}`}
+                    spy={true}
+                    smooth={true}
+                    offset={-160}
+                    duration={500}
+                    onClick={() => setcurrentTab(index)}
+                  >{section.tabName}</Link>
+                </div>
+              )
+            })}
+          </div>
+          <div>
+            {card.sections.map((section, index) => {
+              return (<div key={index}>
+                {/* <a id={`#${index}`} className={styles.anchor} /> */}
+                <div id={`#${index}`} style={{fontWeight: 'bold'}}>{section.sectionName}</div>
+                <div dangerouslySetInnerHTML={{__html: section.sectionText}} />
+              </div>
+              )
+            })}
+          </div>
+        </> : <><div dangerouslySetInnerHTML={{__html: card.description}} /></>}
+        
       </main>
     </MainLayout>
   )
