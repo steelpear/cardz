015dc9f6e9584e47092a5937f10d4462df9bdfd4
steelpear <steelpear@gmail.com>
2023-02-08 11:46:10 +0300

    add ckeditor

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react'
-import {MainLayout} from '../../components/MainLayout'
-import { Editor } from 'primereact/editor'
+import { useState, useEffect, useRef } from 'react'
+import { MainLayout } from '../../components/MainLayout'
 import { Loader } from '../../components/Loader'
 import styles from '@/styles/Edit.module.css'
 
 export default function Edit({card}) {
+  const editorRef = useRef()
+  const [editorLoaded, setEditorLoaded] = useState(false)
+  const { CKEditor, Editor } = editorRef.current || {}
   const [loading, setLoading] = useState(true)
   const [sectionContent, setSectionContent] = useState('')
   const [currentTab, setcurrentTab] = useState(0)
@@ -14,6 +16,11 @@ export default function Edit({card}) {
     if (card.sections && card.sections.length > 0) {
       setSectionContent(card.sections[0].sectionText)
     } else {setSectionContent(card.description)}
+    editorRef.current = {
+      CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
+      Editor: require('ckeditor5-custom-build/build/ckeditor')
+    }
+    setEditorLoaded(true)
   },[])
 
   if (loading) {return (<Loader />)}
@@ -23,7 +30,7 @@ export default function Edit({card}) {
     setSectionContent(card.sections[index].sectionText)
   }
 
-  return(
+  return editorLoaded ? (
     <MainLayout>
       <main className={styles.main}>
         <h1 style={{textAlign:'center', marginTop: '55px'}}>{card.name}</h1>
@@ -37,16 +44,19 @@ export default function Edit({card}) {
               )
             })}
           </div>
-          <div className="card">
-            <Editor value={sectionContent} onTextChange={(e) => setSectionContent(e.htmlValue)} style={{ height: '320px' }} />
-          </div>
-        </> : <>
+        </> : <></>}
         <div className="card">
-          <Editor value={sectionContent} onTextChange={(e) => setSectionContent(e.htmlValue)} style={{ height: '320px' }} />
-        </div></>}
+          <CKEditor
+            editor={ Editor  }
+            data={ sectionContent }
+            onReady={ editor => {console.log( 'Editor is ready to use!', editor )}}
+            onBlur={ ( event, editor ) => {console.log( 'Blur.', editor )}}
+            onFocus={ ( event, editor ) => {console.log( 'Focus.', editor )}}
+          />
+        </div>
       </main>
     </MainLayout>
-  )
+  ) : (<MainLayout><div>Editor loading</div></MainLayout>)
 }
 
 export const getServerSideProps = async (context) => {
